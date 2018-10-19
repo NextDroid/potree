7206c94ace90f357ca7a761f4c5f9b77c7eabe27
Vinay <vrajur@nextdroidsystems.com>
2018-10-19 12:38:07 -0400

    added placeholder clouds for streaming vis

---
+++ pointclouds/placeholders/cube-cloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 9,
+    "boundingBox": {
+        "lx": -1.0,
+        "ly": -1.0,
+        "lz": -1.0,
+        "ux": 1.0,
+        "uy": 1.0,
+        "uz": 1.0
+    },
+    "tightBoundingBox": {
+        "lx": -1.0,
+        "ly": -1.0,
+        "lz": -1.0,
+        "ux": 1.0,
+        "uy": 1.0,
+        "uz": 1.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.017320508137345315,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
