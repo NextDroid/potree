ddbd3afe0e4b7223dfb5cddbb406ed1cdaab96ac
Nick Rizzo <nrizzo@nextdroid.com>
2020-02-24 15:34:37 -0500

    Moved shader var into paramLoader

--- demo/loadController.js
+++ demo/loadController.js
@@ -1,11 +1,12 @@
 // this file is intended to call every other function in order to get potree to load
 
-import { runForLocalDevelopment, params, bucket, region, names, name, visualizationMode,
-	annotateLanesAvailable, downloadLanesAvailable, calibrationModeAvailable, accessKeyId,
-	secretAccessKey, sessionToken, fonts, theme, comparisonDatasets, s3} from "../demo/paramLoader.js"
+import {} from "../demo/paramLoader.js"
 import { createViewer } from "../demo/viewer.js" 
 
 // Call all function to load potree
 export function loadPotree() {
-	createViewer();
+    createViewer();
+    
+    // Create AnimationEngine:
+    window.animationEngine = new AnimationEngine();
 }
